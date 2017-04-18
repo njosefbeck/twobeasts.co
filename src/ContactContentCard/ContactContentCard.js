4b28a4e4fd6d9212be64af4f64d9c28a5feee906
Nathan Beck <nathan.beck@fleishman.com>
2017-04-18 17:00:32 -0500

    Add additional pages

--- src/ContactContentCard/ContactContentCard.js
+++ src/ContactContentCard/ContactContentCard.js
@@ -1,10 +1,64 @@
 import React, { Component } from 'react';
 
+import 'font-awesome/css/font-awesome.css';
+
+import './ContactContentCard.css';
+
 class ContactContentCard extends Component {
   render() {
     return (
-      <section className="content-card">
+      <section className="content-card contact-card">
         <h2 className="content-header">Contact</h2>
+
+        <ul className="top-social-nav">
+          <li>
+            <a href="https://www.facebook.com/hellotwobeasts" target="_blank">
+              <i className="fa fa-facebook" aria-hidden="true"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.instagram.com/hellotwobeasts" target="_blank">
+              <i className="fa fa-instagram" aria-hidden="true"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.twitter.com/hellotwobeasts" target="_blank">
+              <i className="fa fa-twitter" aria-hidden="true"></i>
+            </a>
+          </li>
+        </ul>
+
+        <p>For general inquiries, please use the form below. If you're interested in discussing a potential project with us, please use <a href="https://goo.gl/forms/vS4u8mmSXmkx2RB03" target="_blank">this form</a> instead.
+        </p>
+
+        <form action="https://formspree.io/hellotwobeasts@gmail.com" method="POST">
+          <input type="text" name="name" placeholder="Name" required />
+          <input type="email" name="_replyto" placeholder="Email" required />
+          <input type="text" name="website" placeholder="Website" />
+          <input type="text" name="_gotcha" style={{ display: 'none' }} />
+          <textarea name="message" defaultValue="Type your message here" required></textarea>
+          <input type="submit" value="Send" /> 
+          <input type="hidden" name="_next" value="/thanks.html" />
+          <input type="hidden" name="_subject" value="New submission!" />
+        </form>
+
+        <ul className="bottom-social-nav">
+          <li>
+            <a href="https://www.facebook.com/hellotwobeasts" target="_blank">
+              <i className="fa fa-facebook" aria-hidden="true"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.instagram.com/hellotwobeasts" target="_blank">
+              <i className="fa fa-instagram" aria-hidden="true"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.twitter.com/hellotwobeasts" target="_blank">
+              <i className="fa fa-twitter" aria-hidden="true"></i>
+            </a>
+          </li>
+        </ul>
       </section>
     )
   }
--- src/ServicesContentCard/ServicesContentCard.js
+++ src/ServicesContentCard/ServicesContentCard.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
 
+import {
+  Link
+} from 'react-router-dom';
+
 class ServicesContentCard extends Component {
   render() {
     return (
       <section className="content-card">
         <h2 className="content-header">Services</h2>
+
+        <p>We offer a range of web design and development services to suit your needs. You can mix and match our services to create a web experience that accomplishes your goals.</p>
+
+        <ul className="content-list">
+          <li>Website and web app design or re-design</li>
+          <li> Website and web app development 
+            <ul className="content-list-child">
+              <li>Static site (personal or business website, portfolio, etc.)</li>
+              <li>Online store</li>
+              <li>Custom WordPress theme</li>
+            </ul>
+          </li>
+          <li> Website/web app maintenance
+            <ul className="content-list-child">
+              <li>Content updates</li>
+              <li>Bug fixes</li>
+            </ul>
+          </li> 
+        </ul>
+
+        <p>We will work with you throughout the design and development process. <Link to='/contact'>Contact us</Link> to tell us about your project.</p>
+
       </section>
     )
   }
